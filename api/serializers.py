643844a02364592568a83f986ad3f58125789d0a
Rafael Neviro <rafaelneviro@outlook.com>
2024-08-15 19:23:17 -0300

    ...

--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from .models import Data
 class DataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Data
-        fields = '__all__'
+        fields = ['boolean_field', 'text_field', 'int_field', 'float_field']
